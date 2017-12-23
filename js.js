8e6b332fc2a68cd4122b2e689a9725d827e23ed3
lore-code <lore.tomasini@gmail.com>
2017-12-23 12:08:46 -0500

    finished js, created random number search

--- js.js
+++ js.js
@@ -21,7 +21,23 @@ $(".types").click(function()
 
    type=$(this).val();
 
+   
+
+});
+
+$(".cat").click(function()
+{
+   
+
+   console.log("something else was clicked");
+   $("#categories").hide();
+
+   $("#type").hide();
+
+   genre=$(this).val();
+
    movieSearch();
+
 
 });
 
@@ -29,18 +45,81 @@ $(".types").click(function()
 
 
 var mykey = '5b71a5423ce9867b54b412be9e53e288';
- 
-var page =  Math.floor(Math.random() * 100);
+
+
+//get the first 50 pages
+var page =  Math.floor(Math.random() * 50);
 
 var type;
 
+var genre;
+
+var genre1;
+
+var url1;
+
+var url2;
+
 
 function movieSearch() {
+
+  //when movie or tv clicked get value 
 
 $(".types").click(function(){
         type = $(this).val();
         
     }); 
+
+//url1 is url with type and key
+
+url1 = "https://api.themoviedb.org/3/discover/"+ type + "?api_key=" + mykey;
+
+//get category value;
+
+$(".cat").click(function(){
+        genre = $(this).val();
+        
+    }); 
+
+
+//genre will give what genre to search for 
+if (genre =="random") {
+
+  //random genres
+
+  url2 = url1 + "&page=" + page + "&language=en-US&certification_country=US&sort_by=popularity.desc&include_adult=false&include_video=false";
+}
+
+if (genre == "drama") {
+
+//genre = drama or history or family or romance
+
+  url2 = url1 + "&page=" + page + "&language=en-US&certification_country=US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=18|36|10751|10749";
+}
+
+if (genre == "comedy") {
+
+//genre = drama or history or family or romance
+
+  url2 = url1 + "&page=" + page + "&language=en-US&certification_country=US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=35"; 
+
+}
+
+if (genre == "action") {
+
+//genre = action or adventure or crime 
+
+  url2 = url1 + "&page=" + page + "&language=en-US&certification_country=US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=28| 12|80|"; 
+
+}
+
+if (genre == "scary") {
+
+//genre = horror or mystery or thriller  
+
+  url2 = url1 + "&page=" + page + "&language=en-US&certification_country=US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=27| 9648|80|53"; 
+
+}
 
 
 
@@ -48,11 +127,15 @@ $(".types").click(function(){
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.themoviedb.org/3/discover/"+ type + "?api_key=" + mykey + "&page=" + page + "&language=en-US&certification_country=US&sort_by=popularity.desc&include_adult=false&include_video=false",
+  "url": url2,
   "headers": {},
   "data": "{}"
   
 }
+
+
+      
+
 
 
 $.ajax(settings).done(function (response) {
@@ -62,14 +145,17 @@ $.ajax(settings).done(function (response) {
 	console.log(total);
   console.log(response);
 
+ //random number using chance library gives me nine numbers between 0 and 19  
+var y = chance.unique(chance.integer, 9, {min: 0, max: 19});
 
+  
+//do this nine times 
   for (var i = 0; i < 9; i++) {
 
-  		var y = Math.floor(Math.random() * 21);
+  		
 
-
-  	
-  		var info = response.results[y];
+  	 //get results in to variable then get title, poster, plot, trailer
+  		var info = response.results[y[i]];
   		console.log(info);
 
   		// var test1 = JSON.stringify(test);
@@ -109,8 +195,12 @@ $.ajax(settings).done(function (response) {
       '<div class="col s12 m4 l4"><div class="card"><div class="card-image waves-effect waves-block waves-light">' + 
       '<img class="activator"' + 'src=' + poster + '></div>' + 
     '<div class="card-content"><span class="card-title activator grey-text text-darken-4">' + title + '<i class="material-icons right">more_vert</i></span></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">' + title + '<i class="material-icons right">close</i></span><p>Plot: ' + plot + '</p></div></div>');
+
+      
   	
   }
+
+
   
 
 
